10d4d67fc9ac1b908a977b6ad0fad866b74d0a7a
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-09 13:41:13 -0400

    Make an axios request to get advices and return it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const newAdvice = () => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        // console.log("response", response.data.slip.advice);
+        setAdvice(response.data.slip.advice);
+        console.log("test", advice);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  useEffect(() => {
+    newAdvice();
+  }, []); // eslint-disable-line
+
   return (
     <div>
-      khaled
+      <div>Daily Dose of Advice</div>
+      <div>{advice}</div>
     </div>
   );
 }
