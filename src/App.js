2334a38df825514a005a470c04a5fa67a2d64113
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-09 21:46:42 -0400

    Add icons and implement text to speech

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import axios from "axios";
 import "./App.scss";
 
 function App() {
-  const [advice, setAdvice] = useState("");
+  const [advice, setAdvice] = useState("Loading...");
+  const msg = new SpeechSynthesisUtterance()
 
   const newAdvice = () => {
     axios.get("https://api.adviceslip.com/advice")
@@ -22,13 +23,22 @@ function App() {
     newAdvice();
   }, []); // eslint-disable-line
 
+  const speechHandler = (msg) => {
+    msg.text = advice
+    window.speechSynthesis.speak(msg)
+  }
+
   return (
     <div className="main-load">
       <div className="content">
         <div className="title">Daily Dose of Advice</div>
         <div className="advice-and-button">
           <div className="advice">{advice}</div>
-          <button className="button" onClick={() => {newAdvice()}}>Give me advice!</button>
+          <div className="icons-with-button">
+          <i onClick={() => speechHandler(msg)} className="fa fa-microphone icons"></i>
+          <button className="button" onClick={() => { newAdvice(); }}>Give me advice!</button>
+          <i className="fa fa-copy icons" />
+          </div>
         </div>
       </div>
     </div>
