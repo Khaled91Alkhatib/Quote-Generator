75991d8450d0e1af1ef5c739f56ab18a5825a801
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-10 13:30:36 -0400

    Add toast msg on copy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 import "./App.scss";
 
 function App() {
   const [advice, setAdvice] = useState("Loading...");
-  const msg = new SpeechSynthesisUtterance()
+  const msg = new SpeechSynthesisUtterance();
+  const notify = () => toast.success("Copied!");
+
 
   const newAdvice = () => {
     axios.get("https://api.adviceslip.com/advice")
@@ -24,9 +28,14 @@ function App() {
   }, []); // eslint-disable-line
 
   const speechHandler = (msg) => {
-    msg.text = advice
-    window.speechSynthesis.speak(msg)
-  }
+    msg.text = advice;
+    window.speechSynthesis.speak(msg);
+  };
+
+  const onCopyClick = () => {
+    navigator.clipboard.writeText(advice);
+    notify();
+  };
 
   return (
     <div className="main-load">
@@ -35,12 +44,13 @@ function App() {
         <div className="advice-and-button">
           <div id="toClip" className="advice">{advice}</div>
           <div className="icons-with-button">
-          <i onClick={() => speechHandler(msg)} className="fa fa-microphone icons"></i>
-          <button className="button" onClick={() => { newAdvice(); }}>Give me advice!</button>
-          <i onClick={() =>  navigator.clipboard.writeText(advice)} className="fa fa-copy icons" />
+            <i onClick={() => speechHandler(msg)} className="fa fa-microphone icons"></i>
+            <button className="button" onClick={() => { newAdvice(); }}>Give me advice!</button>
+            <i onClick={onCopyClick} className="fa fa-copy icons" />
           </div>
         </div>
       </div>
+      <ToastContainer autoClose={1000} />
     </div>
   );
 }
